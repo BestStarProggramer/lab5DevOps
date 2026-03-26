7ed6b4e5805d81cb65230ba94a22a996f9e2961b
Dmitriy <3sheepcanfly3@gmail.com>
2026-03-26 09:49:58 +0300

    tests3

--- tests/test_user.py
+++ tests/test_user.py
@@ -34,7 +34,7 @@ def test_create_user_with_valid_email():
     }
     response = client.post("/api/v1/user", json=new_user)
     assert response.status_code == 201
-    assert response.json() == new_user
+    assert response.json() == 3
 
 def test_create_user_with_invalid_email():
     duplicate_user = {
@@ -44,13 +44,12 @@ def test_create_user_with_invalid_email():
     }
     response = client.post("/api/v1/user", json=duplicate_user)
     assert response.status_code == 409
-    assert response.json() == {'detail': 'User already exists'}
+    assert response.json() == {'detail': 'User with this email already exists'}
 
 def test_delete_user():
     user_email = users[1]['email']
     response = client.delete("/api/v1/user", params={'email': user_email})
     assert response.status_code == 204
-    assert response.json() == {'message': f'User {user_email} deleted'}
-    
+
     get_response = client.get("/api/v1/user", params={'email': user_email})
     assert get_response.status_code == 404
